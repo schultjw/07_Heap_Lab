c7473e93eeb62d2f6df741eba8bc435650cb27da
unknown <schultjw@EAS-BEN016T11.it.muohio.edu>
2014-12-10 23:35:26 -0500

    Coded in Heap, answered in README

--- Heap.h
+++ Heap.h
@@ -46,4 +46,88 @@ private:
   void trickleDown(unsigned long index);  
 };
 
-#include "Heap.ipp"
+//////////////////////////////////#include "Heap.ipp"
+
+#include <string>
+
+template<class Pri, class T>
+Heap<Pri,T>::Heap(){
+  arrSize = START_SIZE;
+  numItems=0;
+  backingArray = new std::pair<Pri, T>[arrSize];
+}
+
+template<class Pri, class T>
+Heap<Pri,T>::~Heap(){
+  delete[] backingArray;
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::grow(){
+  std::pair<Pri, T>* newArray = new std::pair<Pri,T>[2*arrSize];
+  for(int k = 0; k < numItems; k++){
+	  newArray[k] = backingArray[k];
+  }
+  arrSize = arrSize * 2;
+  delete[] backingArray;
+  backingArray = newArray;
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::add(std::pair<Pri,T> toAdd){
+  if(numItems + 1 > arrSize) 
+	  grow();
+  backingArray[numItems] = toAdd;
+  bubbleUp(numItems);
+  numItems++;
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::bubbleUp(unsigned long index){
+	int pindex = (index - 1) / 2;
+	if(backingArray[pindex].first > backingArray[index].first && index > 0){
+		backingArray[pindex].swap(backingArray[index]);
+		index = pindex;
+		bubbleUp(pindex);
+	}
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::trickleDown(unsigned long index){
+	int i = index;
+
+	do{
+		int j = -1;
+		int rdex = (index * 2) + 2;
+		if((rdex < numItems) && (backingArray[rdex].first < backingArray[i].first)){
+			int ldex = (index * 2) + 1;
+			if(backingArray[ldex].first < backingArray[rdex].first)
+				j = ldex;
+			else
+				j = rdex;
+		}else {
+			int ldex = (index * 2) + 1;
+			if(ldex < numItems && (backingArray[ldex].first < backingArray[i].first))
+				j = ldex;
+		}
+
+		if(j >= 0)
+			backingArray[i].swap(backingArray[j]);
+		i=j;
+	}while(i>=0);
+}
+
+template<class Pri, class T>
+std::pair<Pri,T> Heap<Pri,T>::remove(){
+  std::pair<Pri, T> ret = backingArray[0];
+  numItems--;
+  backingArray[0] = backingArray[numItems];
+  trickleDown(0);
+  return ret;
+}
+
+template<class Pri, class T>
+unsigned long Heap<Pri,T>::getNumItems(){
+	return numItems;
+}
+
--- README.md
+++ README.md
@@ -18,12 +18,19 @@ Questions
 
 #### 1. Which of the above requirements work, and which do not? For each requirement, write a brief response.
 
-1. TODO
-2. TODO
+1. Ought to work based on reading.
+2. Works. delete[] called when necessary.
 
 #### 2. Exercises 10.1 and 10.2 from http://opendatastructures.org/ods-cpp/10_3_Discussion_Exercises.html
-TODO
-
+10.1 Add 7 as a child of 16. begin swapping upwards.
+		7 and 16 swap. 7 > 6, so we're done.
+	Add 3 as child of 7 (in its final spot). Begin swapping upwards.
+		3 and 7 swap.
+		3 and 6 swap.
+		3 and 4 swap.
+	Order is now:
+	3 9 4 17 26 8 6 19 69 32 93 55 50 16 7
+10.2
 #### 3. Exercise 10.4 from http://opendatastructures.org/ods-cpp/10_3_Discussion_Exercises.html
 
 #### 4. What is one question that confused you about this exercise, or one piece of advice you would share with students next semester?
